6df5d72fcf0d00ba60e0dddfc8eec0de0828bbfb
quanlyvps999 <quanlyvps999@gmail.com>
2025-09-06 23:13:25 +0700

    Update ROLLTRUNG.lua

--- ROLLTRUNG.lua
+++ ROLLTRUNG.lua
@@ -13,13 +13,13 @@ local StartRolls = false
 
 local function Roll()
     local args = {
-	    "ub_tropical", -- Confusion: ub_tropical, Beehive: ub_bee, Lucky Clover : ub_sun
+	    "ub_bee", -- Confusion: ub_tropical, Beehive: ub_bee, Lucky Clover : ub_sun
 	    10
     }
     game:GetService("ReplicatedStorage"):WaitForChild("RemoteFunctions"):WaitForChild("BuyUnitBox"):InvokeServer(unpack(args))
 	task.wait(0.5)
 	local args = {
-	    "ub_tropical", -- Confusion: ub_tropical, Beehive: ub_bee, Lucky Clover : ub_sun
+	    "ub_sun", -- Confusion: ub_tropical, Beehive: ub_bee, Lucky Clover : ub_sun
 	    10
     }
     game:GetService("ReplicatedStorage"):WaitForChild("RemoteFunctions"):WaitForChild("BuyUnitBox"):InvokeServer(unpack(args))
