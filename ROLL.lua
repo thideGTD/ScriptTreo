7a822422f89aa52cb790f1ef76062bfe1546faec
thideGTD <trongnghiad892@gmail.com>
2025-09-20 00:11:29 +0700

    Update ROLL.lua

--- ROLL.lua
+++ ROLL.lua
@@ -1,5 +1,5 @@
-local SeedWaitRoll = 4500000
-local SeedStopRoll = 4500000
+local SeedWaitRoll = 4500
+local SeedStopRoll = 4500
 local _wait = task.wait
 
 repeat _wait() until game:IsLoaded()
@@ -95,6 +95,7 @@ while true do
 	end
 	_wait(5)
 end
+
 
 
 
