6d6b1bae034b90a4b5e3f815054d720ff4ad2f3d
thideGTD <trongnghiad892@gmail.com>
2025-09-20 17:53:56 +0700

    Update ROLLTRUNG.lua

--- ROLLTRUNG.lua
+++ ROLLTRUNG.lua
@@ -12,31 +12,11 @@ local deleteRemote = ReplicatedStorage:WaitForChild("RemoteFunctions"):WaitForCh
 local StartRolls = false
 
 local function Roll()
-    -- local args = {
-	   --  "ub_tropical",
-	   --  10
-    -- }
-    -- game:GetService("ReplicatedStorage"):WaitForChild("RemoteFunctions"):WaitForChild("BuyUnitBox"):InvokeServer(unpack(args))
 	local args = {
-	     "ub_anime",
+	     "ub_anime", -- ub_sun : clover, ub_bee: beehive, ub_tropical: confusion, ub_anime: chomp
 	     10  
     }
     game:GetService("ReplicatedStorage"):WaitForChild("RemoteFunctions"):WaitForChild("BuyUnitBox"):InvokeServer(unpack(args))
-
-
-	-- task.wait(0.5)
-	--  local args = {
-	--     "ub_sun",
-	--     10
- --    }
- --    game:GetService("ReplicatedStorage"):WaitForChild("RemoteFunctions"):WaitForChild("BuyUnitBox"):InvokeServer(unpack(args))
-	-- task.wait(0.5)
- --    local args = {
-	--     "ub_bee",
-	--     10
- --    }
- --    game:GetService("ReplicatedStorage"):WaitForChild("RemoteFunctions"):WaitForChild("BuyUnitBox"):InvokeServer(unpack(args))
-
 end
 
 local function RemoveUnit()
